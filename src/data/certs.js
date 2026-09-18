b780cecdbcde934d5f380d534b1ce29e8e35dee2
Ameer <emailgithub@email.commuhammad_murtadho_33rpl@student.smktelkom-mlg.sch.id>
2026-09-18 18:19:57 +0700

    update: foto profile and view certificate

--- src/data/certs.js
+++ src/data/certs.js
@@ -1,4 +1,5 @@
 export const certs = [
   { slug: "js-dicoding", name: "Sertifikasi JavaScript", issuer: "Dicoding", img: "/images/certificates/js-dicoding.jpg", initials: "JS" },
-  { slug: "nextjs-nestjs-smk", name: "Website Development using Next.js & Nest.js", issuer: "SMK Telkom Malang", img: "/images/certificates/nextjs-nestjs-smk.jpeg", initials: "Ns" }
+  { slug: "nextjs-nestjs-smk", name: "Website Development using Next.js & Nest.js", issuer: "SMK Telkom Malang", img: "/images/certificates/nextjs-nestjs-smk.jpeg", initials: "Ns" },
+  { slug: "software-engineer-hackerrank", name: "Sertifikasi Software Engineer Intern", issuer: "Hackerrank", img: "/images/certificates/hackerrank.jpg", initials: "SE" },
 ];
